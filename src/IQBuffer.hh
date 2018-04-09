c030936238f771c69f3d7f567634644821db4e1c
Geoffrey Mainland <mainland@apeiron.net>
2018-04-09 15:50:30 -0400

    Make IQBuf a real class.

--- src/IQBuffer.hh
+++ src/IQBuffer.hh
@@ -7,7 +7,71 @@
 #include <vector>
 
 /** A buffer of IQ samples */
-using IQBuf = std::vector<std::complex<float>>;
+class IQBuf {
+public:
+    IQBuf()
+    {
+    }
+
+    IQBuf(size_t sz) :
+        data(sz)
+    {
+    }
+
+    IQBuf(const IQBuf& other) :
+        data(other.data)
+    {
+    }
+
+    IQBuf(IQBuf&& other) noexcept :
+        data(std::move(other.data))
+    {
+    }
+
+    ~IQBuf() noexcept
+    {
+    }
+
+    IQBuf& operator=(const IQBuf& other)
+    {
+        IQBuf tmp(other);
+        *this = std::move(tmp);
+        return *this;
+    }
+
+    IQBuf& operator=(IQBuf&& other) noexcept
+    {
+        if (this == &other)
+            return *this;
+
+        data = std::move(other.data);
+
+        return *this;
+    }
+
+    const std::complex<float>& operator[](int i) const
+    {
+        return data[i];
+    }
+
+    std::complex<float>& operator[](int i)
+    {
+        return data[i];
+    }
+
+    size_t size(void)
+    {
+        return data.size();
+    }
+
+    void resize(size_t sz)
+    {
+        data.resize(sz);
+    }
+
+private:
+    std::vector<std::complex<float>> data;
+};
 
 /** A queue of IQ samples */
 using IQQueue = std::deque<std::shared_ptr<IQBuf>>;
