e45f89189a3360239d0fa78fff3568a8e3253e62
Geoffrey Mainland <mainland@apeiron.net>
2018-05-17 18:10:22 -0400

    Actually use the MultiOFDM PHY when we're asked to.

--- python/fullradio.py
+++ python/fullradio.py
@@ -147,7 +147,7 @@ def main():
     elif args.phy == 'ofdm':
         phy = dragonradio.OFDM(net, args.M, args.cp_len, args.taper_len, min_packet_size)
     elif args.phy == 'multiofdm':
-        phy = dragonradio.OFDM(net, args.M, args.cp_len, args.taper_len, min_packet_size)
+        phy = dragonradio.MultiOFDM(net, args.M, args.cp_len, args.taper_len, min_packet_size)
     else:
         print('Bad PHY: {}'.format(args.phy), file=sys.stderr)
         sys.exit(1)
