4724c968f9ed8cf840c228822d96753f86ec07dd
Geoffrey Mainland <mainland@apeiron.net>
2018-06-03 19:26:06 -0400

    make clean after building dependencies.

--- build.sh
+++ build.sh
@@ -8,16 +8,16 @@ sudo apt install -y libhdf5-dev libhdf5-doc libhdf5-cpp-11 libhdf5-cpp-11-dbg
 sudo apt install -y python3 python3-pip python3-h5py
 
 # Build and install libfec
-(cd dependencies/libfec && ./bootstrap && ./configure && make && sudo make install && sudo ldconfig)
+(cd dependencies/libfec && ./bootstrap && ./configure && make && sudo make install && sudo ldconfig && make clean)
 
 # Build and install UHD
-(cd dependencies/uhd/host && mkdir -p build && cd build && cmake ../ && make && make test && sudo make install && sudo ldconfig)
+(cd dependencies/uhd/host && mkdir -p build && cd build && cmake ../ && make && make test && sudo make install && sudo ldconfig && make clean)
 
 # Build and install liquid-dsp
-(cd dependencies/liquid-dsp && ./bootstrap.sh && ./configure && make && sudo make install && sudo ldconfig)
+(cd dependencies/liquid-dsp && ./bootstrap.sh && ./configure && make && sudo make install && sudo ldconfig && make clean)
 
 # Build and install liquid-usrp
-(cd dependencies/liquid-usrp && ./bootstrap.sh && ./configure && make && sudo make install && sudo ldconfig)
+(cd dependencies/liquid-usrp && ./bootstrap.sh && ./configure && make && sudo make install && sudo ldconfig && make clean)
 
 # Install Python dependencies
 sudo pip3 install --upgrade pip
