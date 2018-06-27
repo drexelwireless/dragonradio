cb7a657a9e1799d3c369313203600fbc94ac0e19
Geoffrey Mainland <mainland@apeiron.net>
2018-06-26 20:12:38 -0400

    Update build script for tools.

--- tools/install-dependencies.sh
+++ tools/install-dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt install python3 python3-pip python3-tk python3-virtualenv
-pip3 install --user --upgrade pip matplotlib numpy scipy ipython jupyter
+pip3 install --user -Ur requirements.txt
