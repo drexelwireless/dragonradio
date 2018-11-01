8068d85d6f0c63c419bbaa22b374180994a07664
Geoffrey Mainland <mainland@apeiron.net>
2018-11-01 16:50:43 -0400

    Put Header object in a separate file.
    This will make it easier to export to python.

--- src/Packet.hh
+++ src/Packet.hh
@@ -15,48 +15,8 @@
 
 #include "buffer.hh"
 #include "Clock.hh"
-#include "Seq.hh"
+#include "Header.hh"
 #include "phy/TXParams.hh"
-
-typedef uint8_t NodeId;
-
-enum {
-    /** @brief Set if the packet is the first in a new connection */
-    kSYN = 0,
-
-    /** @brief Set if the packet is ACKing */
-    kACK,
-
-    /** @brief Set if the packet is NAKing */
-    kNAK,
-
-    /** @brief Set if this is a broadcast packet */
-    kBroadcast,
-
-    /** @brief Set if the packet has control data */
-    kControl
-};
-
-typedef uint16_t PacketFlags;
-
-enum {
-    /** @brief Set if the packet has an assigned sequence number */
-    kHasSeq = 0,
-
-    /** @brief Set if the packet belongs to the internal IP network (10.*) */
-    kIntNet,
-
-    /** @brief Set if the packet belongs to the external IP network (192.168.*) */
-    kExtNet,
-
-    /** @brief Set if the packet had invalid header */
-    kInvalidHeader,
-
-    /** @brief Set if the packet had invalid payload */
-    kInvalidPayload
-};
-
-typedef uint16_t InternalFlags;
 
 /** @brief A time */
 struct Time {
@@ -115,39 +75,6 @@ struct ControlMsg {
         Timestamp timestamp;
         TimestampDelta timestamp_delta;
     };
-};
-
-/** @brief %PHY packet header. */
-struct Header {
-    /** @brief Current hop. */
-    NodeId curhop;
-
-    /** @brief Next hop. */
-    NodeId nexthop;
-
-    /** @brief Packet flags. */
-    PacketFlags flags;
-
-    /** @brief Packet sequence number. */
-    Seq seq;
-
-    /** @brief Length of the packet payload. */
-    /** The packet payload may be padded or contain control data. This field
-     * gives the size of the data portion of the payload.
-     */
-    uint16_t data_len;
-};
-
-/** @brief Extended header that appears in radio payload. */
-struct ExtendedHeader {
-    /** @brief Source */
-    NodeId src;
-
-    /** @brief Destination */
-    NodeId dest;
-
-    /** @brief Sequence number we are ACK'ing or NAK'ing. */
-    Seq ack;
 };
 
 /** @brief A packet. */
