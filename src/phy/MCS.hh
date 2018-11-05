24ac7a4118ad5195d91c7dce052de8496bcd8cf3
Geoffrey Mainland <mainland@apeiron.net>
2018-11-05 15:12:15 -0500

    Get rid of unnecessary equality tests.
    Now that we use the liquid wrappers everywhere, we no longer needs these
    equality tests.

--- src/phy/MCS.hh
+++ src/phy/MCS.hh
@@ -78,19 +78,6 @@ inline void mcs2flexframegenprops(const MCS &mcs, ofdmflexframegenprops_s &props
     props.mod_scheme = mcs.ms;
 }
 
-inline bool operator ==(const ofdmflexframegenprops_s &props, const MCS &mcs)
-{
-    return props.check == mcs.check &&
-           props.fec0 == mcs.fec0 &&
-           props.fec1 == mcs.fec1 &&
-           props.mod_scheme == mcs.ms;
-}
-
-inline bool operator !=(const ofdmflexframegenprops_s &props, const MCS &mcs)
-{
-    return !(props == mcs);
-}
-
 inline void mcs2flexframegenprops(const MCS &mcs, origflexframegenprops_s &props)
 {
     props.check = mcs.check;
@@ -99,38 +86,12 @@ inline void mcs2flexframegenprops(const MCS &mcs, origflexframegenprops_s &props
     props.mod_scheme = mcs.ms;
 }
 
-inline bool operator ==(const origflexframegenprops_s &props, const MCS &mcs)
-{
-    return props.check == mcs.check &&
-           props.fec0 == mcs.fec0 &&
-           props.fec1 == mcs.fec1 &&
-           props.mod_scheme == mcs.ms;
-}
-
-inline bool operator !=(const origflexframegenprops_s &props, const MCS &mcs)
-{
-    return !(props == mcs);
-}
-
 inline void mcs2flexframegenprops(const MCS &mcs, flexframegenprops_s &props)
 {
     props.check = mcs.check;
     props.fec0 = mcs.fec0;
     props.fec1 = mcs.fec1;
     props.mod_scheme = mcs.ms;
-}
-
-inline bool operator ==(const flexframegenprops_s &props, const MCS &mcs)
-{
-    return props.check == mcs.check &&
-           props.fec0 == mcs.fec0 &&
-           props.fec1 == mcs.fec1 &&
-           props.mod_scheme == mcs.ms;
-}
-
-inline bool operator !=(const flexframegenprops_s &props, const MCS &mcs)
-{
-    return !(props == mcs);
 }
 
 #endif /* MCS_HH_ */
