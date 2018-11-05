ccda546c4fba5f110d5534b4cee25b0847eef8a6
Geoffrey Mainland <mainland@apeiron.net>
2018-11-05 12:16:25 -0500

    Don't install Ubuntu's h5py module.
    It's broken, and we use pip to install the current version when we need it
    anyway.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sudo apt install -y autoconf automake
 sudo apt install -y libboost-all-dev libusb-1.0-0-dev python-mako doxygen python-docutils cmake build-essential libncurses5-dev
 sudo apt install -y libfftw3-dev
 sudo apt install -y libhdf5-dev libhdf5-doc libhdf5-cpp-11 libhdf5-cpp-11-dbg
-sudo apt install -y python3 python3-pip python3-h5py
+sudo apt install -y python3 python3-pip
 
 # Build and install libcorrect
 (cd dependencies/libcorrect && rm -rf build && mkdir build && cd build && cmake .. && make && make shim && sudo make install && sudo ldconfig && make clean && cd .. && rm -rf build)
