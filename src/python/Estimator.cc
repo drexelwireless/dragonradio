7f1700ee04f9739fff6b328fde4661d5e6371c89
Geoffrey Mainland <mainland@apeiron.net>
2018-11-22 11:25:18 -0500

    Export more estimators to python.

--- src/python/Estimator.cc
+++ src/python/Estimator.cc
@@ -1,19 +1,56 @@
 #include "python/PyModules.hh"
 #include "stats/Estimator.hh"
 
+template <class T>
+void exportEstimator(py::module &m, const char *name)
+{
+    py::class_<Estimator<T>, std::shared_ptr<Estimator<T>>>(m, name)
+        .def_property_readonly("value",
+            &Estimator<T>::getValue,
+            "The value of the estimator")
+        .def_property_readonly("nsamples",
+            &Estimator<T>::getNSamples,
+            "The number of samples used in the estimate")
+        .def("reset",
+            &Estimator<T>::reset,
+            "Reset the estimate")
+        .def("update",
+            &Estimator<T>::update,
+            "Update the estimate")
+        ;
+}
+
+template <class T>
+void exportMeanEstimator(py::module &m, const char *name)
+{
+    py::class_<Mean<T>, Estimator<T>, std::shared_ptr<Mean<T>>>(m, name)
+        .def(py::init<>())
+        .def(py::init<T>())
+        .def("remove",
+            &Mean<T>::remove,
+            "Remove a value used to estimate the mean")
+        ;
+}
+
+template <class T>
+void exportWindowedMeanEstimator(py::module &m, const char *name)
+{
+    py::class_<WindowedMean<T>, Estimator<T>, std::shared_ptr<WindowedMean<T>>>(m, name)
+        .def_property("window_size",
+            &WindowedMean<T>::getWindowSize,
+            &WindowedMean<T>::setWindowSize,
+            "Number of samples in window")
+        ;
+}
+
 void exportEstimators(py::module &m)
 {
-    // Export estimator classes to Python
-    py::class_<Estimator<float>, std::shared_ptr<Estimator<float>>>(m, "Estimator")
-        .def_property_readonly("value", &Estimator<float>::getValue, "The value of the estimator")
-        .def_property_readonly("nsamples", &Estimator<float>::getNSamples, "The number of samples used in the estimate")
-        .def("reset", &Estimator<float>::reset, "Reset the estimate")
-        .def("update", &Estimator<float>::update, "Update the estimate")
-        ;
+    exportEstimator<float>(m, "FloatEstimator");
+    exportEstimator<double>(m, "DoubleEstimator");
 
-    py::class_<Mean<float>, Estimator<float>, std::shared_ptr<Mean<float>>>(m, "Mean")
-        .def(py::init<>())
-        .def(py::init<float>())
-        .def("remove", &Mean<float>::remove, "Remove a value used to estimate the mean")
-        ;
+    exportMeanEstimator<float>(m, "FloatMean");
+    exportMeanEstimator<double>(m, "DoubleMean");
+
+    exportWindowedMeanEstimator<float>(m, "FloatWindowedMean");
+    exportWindowedMeanEstimator<double>(m, "DoubleWindowedMean");
 }
