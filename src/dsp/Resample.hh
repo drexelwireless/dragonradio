fdd4b2957f64fbad27f4d67c4ded29bd07fbe797
Geoffrey Mainland <mainland@apeiron.net>
2018-12-07 12:04:24 -0500

    Make resamplers templated.

--- src/dsp/Resample.hh
+++ src/dsp/Resample.hh
@@ -5,6 +5,7 @@
 
 #include "IQBuffer.hh"
 
+template <class I, class O>
 class Resampler {
 public:
     Resampler() = default;
@@ -18,9 +19,26 @@ public:
 
     virtual void reset(void) = 0;
 
-    virtual size_t resample(const std::complex<float> *in,
-                            size_t count,
-                            std::complex<float> *out) = 0;
+    virtual size_t resample(const I *in, size_t count, O *out) = 0;
+};
+
+using C = std::complex<float>;
+
+template <>
+class Resampler<C,C> {
+public:
+    Resampler() = default;
+    virtual ~Resampler() = default;
+
+    virtual double getRate(void) const = 0;
+
+    virtual double getDelay(void) const = 0;
+
+    virtual size_t neededOut(size_t count) const = 0;
+
+    virtual void reset(void) = 0;
+
+    virtual size_t resample(const C *in, size_t count, C *out) = 0;
 
     virtual std::shared_ptr<IQBuf> resample(const IQBuf &in)
     {
--- src/python/Resample.cc
+++ src/python/Resample.cc
@@ -5,37 +5,60 @@
 #include "liquid/Resample.hh"
 #include "python/PyModules.hh"
 
-void exportResamplers(py::module &m)
+template <class I, class O>
+void exportResampler(py::module &m, const char *name)
 {
-    // Export class Resampler to Python
-    py::class_<Resampler, std::shared_ptr<Resampler>>(m, "Resampler")
-        .def_property_readonly("rate", &Resampler::getRate, "Resampling rate")
-        .def_property_readonly("delay", &Resampler::getDelay, "Resampling delay")
-        .def("reset", &Resampler::reset, "reset resampler state")
-        .def("resample", [](Resampler &resamp, py::array_t<std::complex<float>> sig) -> py::array_t<std::complex<float>> {
-            auto inbuf = sig.request();
+    using pyarray_I = py::array_t<I, py::array::c_style | py::array::forcecast>;
+    using pyarray_O = py::array_t<O>;
 
-            py::array_t<std::complex<float>> outarr(resamp.neededOut(inbuf.size));
-            auto                             outbuf = outarr.request();
-            unsigned                         nw;
+    py::class_<Resampler<I,O>, std::shared_ptr<Resampler<I,O>>>(m, name)
+        .def_property_readonly("rate",
+            &Resampler<I,O>::getRate,
+            "Resampling rate")
+        .def_property_readonly("delay",
+            &Resampler<I,O>::getDelay,
+            "Resampling delay")
+        .def("reset",
+            &Resampler<I,O>::reset,
+            "Reset resampler state")
+        .def("resample",
+            [](Resampler<I,O> &resamp, pyarray_I sig) -> pyarray_O
+            {
+                auto      inbuf = sig.request();
+                pyarray_O outarr(resamp.neededOut(inbuf.size));
+                auto      outbuf = outarr.request();
+                unsigned  nw;
 
-            nw = resamp.resample(static_cast<std::complex<float>*>(inbuf.ptr),
-                                 inbuf.size,
-                                 static_cast<std::complex<float>*>(outbuf.ptr));
-            assert(nw <= outbuf.size);
+                nw = resamp.resample(static_cast<I*>(inbuf.ptr),
+                                     inbuf.size,
+                                     static_cast<O*>(outbuf.ptr));
+                assert(nw <= outbuf.size);
 
-            outarr.resize({nw});
+                outarr.resize({nw});
 
-            return outarr;
-        }, "resample signal")
+                return outarr;
+            },
+            "Resample signal")
         ;
+}
 
-    // Export class MultiStageResampler to Python
-    py::class_<Liquid::MultiStageResampler, Resampler, std::shared_ptr<Liquid::MultiStageResampler>>(m, "MultiStageResampler")
+template <class I, class O, class C>
+void exportLiquidMSResamp(py::module &m, const char *name)
+{
+    py::class_<Liquid::MultiStageResampler<I,O,C>, Resampler<I,O>, std::shared_ptr<Liquid::MultiStageResampler<I,O,C>>>(m, name)
         .def(py::init<double,
                       unsigned,
                       double,
                       double,
                       unsigned>())
-            ;
+        ;
+}
+
+void exportResamplers(py::module &m)
+{
+    using C = std::complex<float>;
+    using F = float;
+
+    exportResampler<C,C>(m, "ResamplerCC");
+    exportLiquidMSResamp<C,C,F>(m, "LiquidMSResampCCF");
 }
