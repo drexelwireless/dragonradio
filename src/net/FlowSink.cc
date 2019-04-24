3365c35c4b5a637fa24d9a42cb5f0684446385e4
Geoffrey Mainland <mainland@apeiron.net>
2019-04-24 16:15:50 -0400

    Add MGEN packet accessors.

--- src/net/FlowSink.cc
+++ src/net/FlowSink.cc
@@ -17,32 +17,9 @@ bool FlowSink::process(std::shared_ptr<RadioPacket> &pkt)
 
     // Update latency
     const struct mgenhdr *mgenh = pkt->getMGENHdr();
+
     if (mgenh) {
-        int64_t secs;
-        int32_t usecs;
-
-        if (mgenh->version == DARPA_MGEN_VERSION) {
-            const struct darpa_mgenhdr *dmgenh = reinterpret_cast<const struct darpa_mgenhdr*>(mgenh);
-            darpa_mgen_secs_t          hdr_secs;
-            mgen_usecs_t               hdr_usecs;
-
-            std::memcpy(&hdr_secs, reinterpret_cast<const char*>(dmgenh) + offsetof(struct darpa_mgenhdr, txTimeSeconds), sizeof(hdr_secs));
-            std::memcpy(&hdr_usecs, reinterpret_cast<const char*>(dmgenh) + offsetof(struct darpa_mgenhdr, txTimeMicroseconds), sizeof(hdr_usecs));
-
-            secs = ntoh_darpa_mgen_secs(hdr_secs);
-            usecs = ntohl(hdr_usecs);
-        } else {
-            mgen_secs_t  hdr_secs;
-            mgen_usecs_t hdr_usecs;
-
-            std::memcpy(&hdr_secs, reinterpret_cast<const char*>(mgenh) + offsetof(struct mgenhdr, txTimeSeconds), sizeof(hdr_secs));
-            std::memcpy(&hdr_usecs, reinterpret_cast<const char*>(mgenh) + offsetof(struct mgenhdr, txTimeMicroseconds), sizeof(hdr_usecs));
-
-            secs = ntoh_mgen_secs(hdr_secs);
-            usecs = ntohl(hdr_usecs);
-        }
-
-        Clock::time_point t_send = Clock::time_point{static_cast<int64_t>(secs), usecs/1e6};
+        Clock::time_point t_send = mgenh->getTimestamp();
         double            delta = (t_recv - t_send).get_real_secs();
 
         info.latency.update(t_recv, delta);
--- src/net/mgen.h
+++ src/net/mgen.h
@@ -2,6 +2,10 @@
 #define MGEN_HH_
 
 #include <sys/types.h>
+#include <arpa/inet.h>
+#include <byteswap.h>
+
+#include "Clock.hh"
 
 /** @brief DARPA MGEN version number */
 #define DARPA_MGEN_VERSION 4
@@ -19,6 +23,17 @@ typedef uint32_t mgen_secs_t;
 
 typedef uint32_t mgen_usecs_t;
 
+/** @brief A DARPA-variant MGEN packet header */
+struct __attribute__((__packed__)) darpa_mgenhdr {
+   uint16_t messageSize;
+   uint8_t version;
+   uint8_t flags;
+   uint32_t mgenFlowId;
+   uint32_t sequenceNumber;
+   darpa_mgen_secs_t txTimeSeconds;
+   mgen_usecs_t txTimeMicroseconds;
+};
+
 /** @brief An MGEN packet header */
 /** See:
  * https://downloads.pf.itd.nrl.navy.mil/docs/mgen/mgen.html
@@ -31,16 +46,62 @@ struct __attribute__((__packed__)) mgenhdr {
     uint32_t sequenceNumber;
     mgen_secs_t txTimeSeconds;
     mgen_usecs_t txTimeMicroseconds;
-};
 
-struct __attribute__((__packed__)) darpa_mgenhdr {
-   uint16_t messageSize;
-   uint8_t version;
-   uint8_t flags;
-   uint32_t mgenFlowId;
-   uint32_t sequenceNumber;
-   darpa_mgen_secs_t txTimeSeconds;
-   mgen_usecs_t txTimeMicroseconds;
+    uint16_t getMessageSize(void) const
+    {
+        uint16_t temp;
+
+        std::memcpy(&temp, reinterpret_cast<const char*>(this) + offsetof(struct mgenhdr, messageSize), sizeof(temp));
+
+        return ntohs(temp);
+    }
+
+    uint32_t getFlowId(void) const
+    {
+        uint32_t temp;
+
+        std::memcpy(&temp, reinterpret_cast<const char*>(this) + offsetof(struct mgenhdr, mgenFlowId), sizeof(temp));
+
+        return ntohl(temp);
+    }
+
+    uint32_t getSequenceNumber(void) const
+    {
+        uint32_t temp;
+
+        std::memcpy(&temp, reinterpret_cast<const char*>(this) + offsetof(struct mgenhdr, sequenceNumber), sizeof(temp));
+
+        return ntohl(temp);
+    }
+
+    Clock::time_point getTimestamp(void) const
+    {
+        int64_t secs;
+        int32_t usecs;
+
+        if (version == DARPA_MGEN_VERSION) {
+            const struct darpa_mgenhdr *dmgenh = reinterpret_cast<const struct darpa_mgenhdr*>(this);
+            darpa_mgen_secs_t          hdr_secs;
+            mgen_usecs_t               hdr_usecs;
+
+            std::memcpy(&hdr_secs, reinterpret_cast<const char*>(dmgenh) + offsetof(struct darpa_mgenhdr, txTimeSeconds), sizeof(hdr_secs));
+            std::memcpy(&hdr_usecs, reinterpret_cast<const char*>(dmgenh) + offsetof(struct darpa_mgenhdr, txTimeMicroseconds), sizeof(hdr_usecs));
+
+            secs = ntoh_darpa_mgen_secs(hdr_secs);
+            usecs = ntohl(hdr_usecs);
+        } else {
+            mgen_secs_t  hdr_secs;
+            mgen_usecs_t hdr_usecs;
+
+            std::memcpy(&hdr_secs, reinterpret_cast<const char*>(this) + offsetof(struct mgenhdr, txTimeSeconds), sizeof(hdr_secs));
+            std::memcpy(&hdr_usecs, reinterpret_cast<const char*>(this) + offsetof(struct mgenhdr, txTimeMicroseconds), sizeof(hdr_usecs));
+
+            secs = ntoh_mgen_secs(hdr_secs);
+            usecs = ntohl(hdr_usecs);
+        }
+
+        return Clock::time_point{static_cast<int64_t>(secs), usecs/1e6};
+    }
 };
 
 struct __attribute__((__packed__)) mgenaddr {
