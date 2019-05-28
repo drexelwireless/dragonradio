105d2ee1ed3c424847c5c938030b724ca07db42a
Geoffrey Mainland <mainland@apeiron.net>
2019-05-28 15:16:56 -0400

    Make Gain threadsafe.

--- src/phy/Gain.hh
+++ src/phy/Gain.hh
@@ -3,36 +3,47 @@
 
 #include <math.h>
 
+#include <atomic>
+
 class Gain {
 public:
-    Gain(void) : g_(1.0)
+    Gain(void)
+      : g_(1.0)
     {
     }
 
     ~Gain() = default;
 
+    Gain& operator =(const Gain &other)
+    {
+        if (this != &other)
+            g_.store(other.g_.load(std::memory_order_relaxed), std::memory_order_relaxed);
+
+        return *this;
+    }
+
     float getLinearGain(void) const
     {
-        return g_;
+        return g_.load(std::memory_order_relaxed);
     }
 
     void setLinearGain(float g)
     {
-        g_ = g;
+        g_.store(g, std::memory_order_relaxed);
     }
 
     float getDbGain(void) const
     {
-        return 20.0*logf(g_)/logf(10.0);
+        return 20.0*logf(g_.load(std::memory_order_relaxed))/logf(10.0);
     }
 
     void setDbGain(float dB)
     {
-        g_ = powf(10.0f, dB/20.0f);
+        g_.store(powf(10.0f, dB/20.0f), std::memory_order_relaxed);
     }
 
 protected:
-    float g_;
+    std::atomic<float> g_;
 };
 
 #endif /* GAIN_H_ */
