2ee45856fbe1a10d9ab2ce7db43ff63e03aed055
Geoffrey Mainland <mainland@apeiron.net>
2019-05-31 09:27:11 -0400

    Add flag to dump events with wall time instead of time offset.

--- tools/dreventlog.py
+++ tools/dreventlog.py
@@ -20,6 +20,9 @@ def main():
     parser.add_argument('-v', '--verbose', action='store_const', const=logging.INFO,
                         dest='loglevel',
                         help='be verbose')
+    parser.add_argument('--wall-time', action='store_true',
+                        default=False,
+                        help='show wall time instead of offset from start')
     parser.add_argument('--events', action='store_true',
                         default=False,
                         help='show events')
@@ -40,23 +43,28 @@ def main():
             log = drlog.Log(recv=args.recv, send=args.send)
             node = log.load(path)
 
+            if args.wall_time:
+                offset = node.log_attrs['start']
+            else:
+                offset = 0
+
             if args.events:
                 print('# timestamp event')
                 events = log.events[node.node_id]
                 for index, row in events.iterrows():
-                    print('{:5.4f}: {}'.format(row[0], row[1]))
+                    print('{:5.4f}: {}'.format(row[0] + offset, row[1]))
 
             if args.recv:
                 print('# timestamp curhop nexthop seq ms')
                 recv = log.received[node.node_id]
                 for index, row in recv.iterrows():
-                    print('{:5.4f}: {} {} {} {}'.format(row.timestamp, row.curhop, row.nexthop, row.seq, row.ms))
+                    print('{:5.4f}: {} {} {} {}'.format(row.timestamp + offset, row.curhop, row.nexthop, row.seq, row.ms))
 
             if args.send:
                 print('# timestamp curhop nexthop seq')
                 send = log.sent[node.node_id]
                 for index, row in send.iterrows():
-                    print('{:5.4f}: {} {} {}'.format(row.timestamp, row.curhop, row.nexthop, row.seq))
+                    print('{:5.4f}: {} {} {}'.format(row.timestamp + offset, row.curhop, row.nexthop, row.seq))
         except:
             logging.exception("Could not load '%s'", path)
 
