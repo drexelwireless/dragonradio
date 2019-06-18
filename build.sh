c2087e8bbd5ca2d2e1a5beae836c6b452aa7fc9e
Geoffrey Mainland <mainland@apeiron.net>
2019-06-18 17:36:50 -0400

    Build firpm library as part of build process.

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ sudo apt install -y libfftw3-dev
 sudo apt install -y libhdf5-dev libhdf5-doc libhdf5-cpp-11 libhdf5-cpp-11-dbg
 sudo apt install -y python3 python3-pip
 sudo apt install -y libflac8 libflac-dev libflac++-dev
+sudo apt install -y libeigen3-dev
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
@@ -24,6 +25,9 @@ CFLAGS="-Ofast -march=native"
 
 # Build and install liquid-dsp
 (cd dependencies/liquid-dsp && ./bootstrap.sh && CC="$CC" CXX="$CXX" CFLAGS="$CFLAGS" ./configure && make && sudo make install && sudo ldconfig && make clean)
+
+# Build and install firpm
+(cd dependencies/firpm/firpm_d && rm -rf build && mkdir build && cd build && cmake .. && make -j4 && sudo make install && sudo ldconfig && make clean && cd .. && rm -rf build)
 
 # Install Python dependencies
 sudo pip3 install --upgrade pip
