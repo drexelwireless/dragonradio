5957403dced956dce2b103b5d5440f46444cabe8
Geoffrey Mainland <mainland@apeiron.net>
2019-07-23 11:38:02 -0400

    Update Scrimmage 5 submission script for final submission.

--- config/phase3/scrimmage5/submit.sh
+++ config/phase3/scrimmage5/submit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 EVENT=scrimmage5
 VALIDATE=5
-IMAGE=cil-20190715-cd79938f
-QUAL=94663
+IMAGE=cil-20190722-e072a033
+QUAL=96278
 
 ssh sc2-lz "rm -rf /share/nas/dragon-radio/$EVENT"
 ssh sc2-lz "mkdir -p /share/nas/dragon-radio/$EVENT/config"
@@ -12,6 +12,11 @@ ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/config/batch.json"
 
 scp "$EVENT.conf" "sc2-lz:/share/nas/dragon-radio/$EVENT/config"
 ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/config/$EVENT.conf"
+
+if [ -f README.md ]; then
+    scp README.md "sc2-lz:/share/nas/dragon-radio/$EVENT/README.md"
+    ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/README.md"
+fi
 
 ssh sc2-lz "cp /share/nas/dragon-radio/images/$IMAGE.tar.gz /share/nas/dragon-radio/$EVENT/$EVENT.tar.gz"
 ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/$EVENT.tar.gz"
