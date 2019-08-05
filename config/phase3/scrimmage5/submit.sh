1c220a75ba187e116e4e5b124fb3d2a1c80c9b5e
Geoffrey Mainland <mainland@apeiron.net>
2019-08-05 19:03:51 -0400

    Update submission for scrimmage 5.1.

--- config/phase3/scrimmage5/submit.sh
+++ config/phase3/scrimmage5/submit.sh
@@ -4,19 +4,22 @@ VALIDATE=5
 IMAGE=cil-20190722-e072a033
 QUAL=96278
 
+copy()
+{
+  FILE=$1; shift
+  DIR=$1; shift
+  PERMISSIONS=$1; shift
+
+  scp "$FILE" "sc2-lz:/share/nas/dragon-radio/$EVENT/$DIR"
+  ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/$DIR$FILE"
+}
+
 ssh sc2-lz "rm -rf /share/nas/dragon-radio/$EVENT"
 ssh sc2-lz "mkdir -p /share/nas/dragon-radio/$EVENT/config"
 
-scp batch.json "sc2-lz:/share/nas/dragon-radio/$EVENT/config"
-ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/config/batch.json"
-
-scp "$EVENT.conf" "sc2-lz:/share/nas/dragon-radio/$EVENT/config"
-ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/config/$EVENT.conf"
-
-if [ -f README.md ]; then
-    scp README.md "sc2-lz:/share/nas/dragon-radio/$EVENT/README.md"
-    ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/README.md"
-fi
+copy "batch.json" "config/" "0750"
+copy "$EVENT.conf" "config/" "0750"
+copy "rf_threshold_baseline.txt" "" "0750"
 
 ssh sc2-lz "cp /share/nas/dragon-radio/images/$IMAGE.tar.gz /share/nas/dragon-radio/$EVENT/$EVENT.tar.gz"
 ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/$EVENT.tar.gz"
