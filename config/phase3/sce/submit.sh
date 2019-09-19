3700b2bcf9377971ae4813a72d53a85d1483a094
Geoffrey Mainland <mainland@apeiron.net>
2019-09-19 12:43:14 -0400

    Add SCE submission script.

---
+++ config/phase3/sce/submit.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+EVENT=sce1
+IMAGE=testing-20190919-7a9cdb90
+QUAL=107368
+
+copy()
+{
+  FILE=$1; shift
+  DIR=$1; shift
+  PERMISSIONS=$1; shift
+
+  scp "$FILE" "sc2-lz:/share/nas/dragon-radio/$EVENT/$DIR"
+  ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/$DIR$FILE"
+}
+
+ssh sc2-lz "rm -rf /share/nas/dragon-radio/$EVENT"
+ssh sc2-lz "mkdir -p /share/nas/dragon-radio/$EVENT/config"
+
+copy "batch.json" "config/" "0750"
+copy "$EVENT.conf" "config/" "0750"
+copy "rf_threshold_baseline.txt" "" "0750"
+
+ssh sc2-lz "cp /share/nas/dragon-radio/images/$IMAGE.tar.gz /share/nas/dragon-radio/$EVENT/$EVENT.tar.gz"
+ssh sc2-lz "chmod 0750 /share/nas/dragon-radio/$EVENT/$EVENT.tar.gz"
+
+ssh sc2-lz "md5sum /share/nas/dragon-radio/images/$IMAGE.tar.gz >/share/nas/dragon-radio/$EVENT/$EVENT.md5"
+
+ssh sc2-lz "cp /share/nas/dragon-radio/RESERVATION-$QUAL/traffic_logs/*.drc /share/nas/dragon-radio/$EVENT/"
+
+ssh sc2-lz "/share/nas/common/other/scrimmage-utils/validate_sce_submission.sh"
