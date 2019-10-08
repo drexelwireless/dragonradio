8ea4575c4664db1ef10db221c77e06bba0da52e6
Geoffrey Mainland <mainland@apeiron.net>
2019-10-08 11:50:08 -0400

    Move ModPacket definition to PHY.hh.

--- src/phy/PHY.hh
+++ src/phy/PHY.hh
@@ -11,7 +11,30 @@
 #include "RadioConfig.hh"
 #include "SafeQueue.hh"
 #include "mac/Snapshot.hh"
-#include "phy/ModPacket.hh"
+
+/** @brief A modulated data packet to be sent over the radio */
+struct ModPacket
+{
+    /** @brief Channel */
+    Channel channel;
+
+    /** @brief Offset of start of packet from start of slot, in number of
+     * samples.
+     */
+    size_t start;
+
+    /** @brief Offset of start of packet from beginning of sample buffer */
+    size_t offset;
+
+    /** @brief Number of modulated samples */
+    size_t nsamples;
+
+    /** @brief Buffer containing the modulated samples. */
+    std::shared_ptr<IQBuf> samples;
+
+    /** @brief The un-modulated packet. */
+    std::shared_ptr<NetPacket> pkt;
+};
 
 /** @brief A physical layer protocol that can provide a modulator and
  * demodulator.
