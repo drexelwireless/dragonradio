0a6d5f1ec50477ec61b579339e9e9ce62ae6dbff
Geoffrey Mainland <mainland@apeiron.net>
2020-07-22 17:36:52 -0400

    drgui et al need HDF5.

--- tools/install.sh
+++ tools/install.sh
@@ -4,6 +4,7 @@ set -e
 sudo apt install -y autoconf automake
 sudo apt install -y libboost-all-dev libusb-1.0-0-dev python-mako doxygen python-docutils cmake build-essential libncurses5-dev
 sudo apt install -y libfftw3-dev
+sudo apt install -y libhdf5-dev libhdf5-doc
 sudo apt install -y python3 python3-pip python3-tk python3-virtualenv virtualenv
 sudo apt install -y libflac8 libflac-dev libflac++-dev
 sudo apt install -y libffi-dev libgirepository1.0-dev libcairo2-dev pkg-config gir1.2-gtk-3.0
