9bfe3c4eb1cfda9f666bb675cb8b4cdb5d336766
Geoffrey Mainland <mainland@apeiron.net>
2021-08-19 10:43:43 -0400

    Fix dumpevents script.

--- tools/bin/dumpevents.py
+++ tools/bin/dumpevents.py
@@ -8,6 +8,7 @@ import logging
 import pandas as pd
 
 from dragonradio.tools.logging import LogCollection
+from dragonradio.tools.logging.command_line import Command
 
 def pprSend(row):
     result = f"SEND: curhop={row.curhop:d}; nexthop={row.nexthop:d}; seq={row.seq:d}"
@@ -23,83 +24,52 @@ def pprSend(row):
 def pprRecv(row):
     return f"RECV: curhop={row.curhop:d}; nexthop={row.nexthop:d}; seq={row.seq:d}"
 
-def main():
-    parser = argparse.ArgumentParser(description='Display DragonRadio event log.')
-    parser.add_argument('-d', '--debug', action='store_const', const=logging.DEBUG,
-                        dest='loglevel',
-                        default=logging.WARNING,
-                        help='print debugging information')
-    parser.add_argument('-v', '--verbose', action='store_const', const=logging.INFO,
-                        dest='loglevel',
-                        help='be verbose')
-    parser.add_argument('--start-time', type=float,
-                        default=None,
-                        metavar='SEC',
-                        help='set start time in seconds since the epoch')
-    parser.add_argument('--wall-time', action='store_true',
-                        default=False,
-                        help='show wall time instead of offset from start')
-    parser.add_argument('--events', action='store_true',
-                        default=False,
-                        help='show events')
-    parser.add_argument('--send', action='store_true',
-                        default=False,
-                        help='show sent packets')
-    parser.add_argument('--recv', action='store_true',
-                        default=False,
-                        help='show received packets')
+class DumpEventsCommand(Command):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    parser.add_argument('-n', '--node', action='append',
-                        metavar='NODE')
+    def add_arguments(self, parser):
+        parser.add_argument('--wall-time', action='store_true',
+                            default=False,
+                            help='show wall time instead of offset from start')
+        parser.add_argument('--events', action='store_true',
+                            default=False,
+                            help='show events')
+        parser.add_argument('--send', action='store_true',
+                            default=False,
+                            help='show sent packets')
+        parser.add_argument('--recv', action='store_true',
+                            default=False,
+                            help='show received packets')
 
-    parser.add_argument('paths', nargs='*')
+    def handle(self, parser, args):
+        node_ids = sorted(self.logs.nodes)
 
-    # Parse arguments
-    try:
-        args = parser.parse_args()
-    except SystemExit as ex:
-        return ex.code
+        for node_id in reversed(node_ids):
+            frames = []
 
-    # Set up logging
-    logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s:%(message)s',
-                        level=args.loglevel)
+            if args.events:
+                frames.append(pd.DataFrame({ 'timestamp': self.logs[node_id].events.timestamp
+                                           , 'event': self.logs[node_id].events.event}))
 
-    # Determine start time
-    if args.start_time:
-        start = datetime.datetime.fromtimestamp(args.start_time, UTC)
-    else:
-        start = None
+            if args.send:
+                frames.append(pd.DataFrame({ 'timestamp': self.logs[node_id].send.timestamp
+                                           , 'event': self.logs[node_id].send.apply(pprSend, axis=1)}))
 
-    # Load logs
-    logs = LogCollection(start=start)
-    logs.load(args.paths)
+            if args.recv:
+                frames.append(pd.DataFrame({ 'timestamp': self.logs[node_id].recv.timestamp
+                                           , 'event': self.logs[node_id].recv.apply(pprRecv, axis=1)}))
 
-    for node_id in args.node:
-        node_id = int(node_id)
+            df = pd.concat(frames)
 
-        frames = []
+            if not args.wall_time:
+                df.timestamp += self.logs[node_id].delta
 
-        if args.events:
-            frames.append(pd.DataFrame({ 'timestamp': logs[node_id].events.timestamp
-                                       , 'event': logs[node_id].events.event}))
+            df = df.sort_values(by='timestamp')
 
-        if args.send:
-            frames.append(pd.DataFrame({ 'timestamp': logs[node_id].send.timestamp
-                                       , 'event': logs[node_id].send.apply(pprSend, axis=1)}))
-
-        if args.recv:
-            frames.append(pd.DataFrame({ 'timestamp': logs[node_id].recv.timestamp
-                                       , 'event': logs[node_id].recv.apply(pprRecv, axis=1)}))
-
-        df = pd.concat(frames)
-
-        if not args.wall_time:
-            df.timestamp += logs[node_id].delta
-
-        df = df.sort_values(by='timestamp')
-
-        for _, (t, event) in df.iterrows():
-            print(f'{t:5.4f}: {event}')
+            for _, (t, event) in df.iterrows():
+                print(f'{t:5.4f}: {event}')
 
 if __name__ == '__main__':
-    main()
+    cmd = DumpEventsCommand()
+    cmd.run('Dump DragonRadio events')
