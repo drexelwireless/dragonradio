df0f7bc6450494660e0a3d69bcc3090294d76c9d
Geoffrey Mainland <mainland@apeiron.net>
2021-08-23 22:34:25 +0000

    Dump timestamps to CSV instead of logging them.

--- python/dragonradio/dragonradio/radio/timesync.py
+++ python/dragonradio/dragonradio/radio/timesync.py
@@ -26,10 +26,6 @@ def synchronize(config, radio, master, me):
     if len(master_timestamps) == 0:
         return
 
-    if True:
-        logger.debug("TIMESYNC: our timestamps:\n%s", pformat(me_timestamps))
-        logger.debug("TIMESYNC: time master's timestamps:\n%s", pformat(master_timestamps))
-
     # If we have a GPSDO, then assume skew is zero
     if config.clock_noskew or \
         (radio.usrp.clock_source == 'external' and radio.usrp.time_source == 'external'):
