a96ed9ef1b5b098d66ebfc59bf3efd499ef80708
Geoffrey Mainland <mainland@apeiron.net>
2021-08-25 14:06:48 +0000

    Simplify python code for SmartController export.

--- src/python/Controller.cc
+++ src/python/Controller.cc
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Drexel University
+// Copyright 2018-2021 Drexel University
 // Author: Geoffrey Mainland <mainland@drexel.edu>
 
 #include <pybind11/pybind11.h>
@@ -7,8 +7,159 @@
 #include "llc/Controller.hh"
 #include "llc/DummyController.hh"
 #include "llc/SmartController.hh"
-#include "llc/SmartController/proxies.hh"
 #include "python/PyModules.hh"
+
+/** @brief A proxy object for a SmartController send window */
+class SendWindowProxy
+{
+public:
+    SendWindowProxy(std::shared_ptr<SmartController> controller,
+                    NodeId node_id)
+      : controller_(controller)
+      , node_id_(node_id)
+    {
+    }
+
+    std::optional<double> getShortPER(void)
+    {
+        SendWindowGuard sendw(*controller_, node_id_);
+
+        return sendw->short_per.value();
+    }
+
+    std::optional<double> getLongPER(void)
+    {
+        SendWindowGuard sendw(*controller_, node_id_);
+
+        return sendw->long_per.value();
+    }
+
+    std::optional<double> getShortEVM(void)
+    {
+        SendWindowGuard sendw(*controller_, node_id_);
+
+        return sendw->short_evm;
+    }
+
+    std::optional<double> getLongEVM(void)
+    {
+        SendWindowGuard sendw(*controller_, node_id_);
+
+        return sendw->long_evm;
+    }
+
+    std::optional<double> getShortRSSI(void)
+    {
+        SendWindowGuard sendw(*controller_, node_id_);
+
+        return sendw->short_rssi;
+    }
+
+    std::optional<double> getLongRSSI(void)
+    {
+        SendWindowGuard sendw(*controller_, node_id_);
+
+        return sendw->long_rssi;
+    }
+
+private:
+    /** @brief This send window's SmartController */
+    std::shared_ptr<SmartController> controller_;
+
+    /** @brief This send window's node ID */
+    const NodeId node_id_;
+};
+
+/** @brief A proxy object for SmartController's send windows */
+class SendWindowsProxy
+{
+public:
+    SendWindowsProxy(std::shared_ptr<SmartController> controller)
+      : controller_(controller)
+    {
+    }
+
+    SendWindowsProxy() = delete;
+    ~SendWindowsProxy() = default;
+
+    SendWindowProxy operator [](NodeId node)
+    {
+        return SendWindowProxy(controller_, node);
+    }
+
+private:
+    /** @brief This object's SmartController */
+    std::shared_ptr<SmartController> controller_;
+};
+
+/** @brief A proxy object for a SmartController receive window */
+class ReceiveWindowProxy
+{
+public:
+    ReceiveWindowProxy(std::shared_ptr<SmartController> controller,
+                       NodeId node_id)
+      : controller_(controller)
+      , node_id_(node_id)
+    {
+    }
+
+    std::optional<double> getShortEVM(void)
+    {
+        RecvWindowGuard recvw(*controller_, node_id_);
+
+        return recvw->short_evm.value();
+    }
+
+    std::optional<double> getLongEVM(void)
+    {
+        RecvWindowGuard recvw(*controller_, node_id_);
+
+        return recvw->long_evm.value();
+    }
+
+    std::optional<double> getShortRSSI(void)
+    {
+        RecvWindowGuard recvw(*controller_, node_id_);
+
+        return recvw->short_rssi.value();
+    }
+
+    std::optional<double> getLongRSSI(void)
+    {
+        RecvWindowGuard recvw(*controller_, node_id_);
+
+        return recvw->long_rssi.value();
+    }
+
+private:
+    /** @brief This send window's SmartController */
+    std::shared_ptr<SmartController> controller_;
+
+    /** @brief This send window's node ID */
+    const NodeId node_id_;
+};
+
+/** @brief A proxy object for SmartController's receive windows */
+class ReceiveWindowsProxy
+{
+public:
+    ReceiveWindowsProxy(std::shared_ptr<SmartController> controller)
+      : controller_(controller)
+    {
+    }
+
+    ReceiveWindowsProxy() = delete;
+    ~ReceiveWindowsProxy() = default;
+
+    ReceiveWindowProxy operator [](NodeId node)
+    {
+        return ReceiveWindowProxy(controller_, node);
+    }
+
+private:
+    /** @brief This object's SmartController */
+    std::shared_ptr<SmartController> controller_;
+};
 
 void exportControllers(py::module &m)
 {
