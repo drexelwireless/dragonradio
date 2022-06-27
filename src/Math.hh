55e6c3402733129bc5c0d9b03c9bed85572b8f7a
Geoffrey Mainland <mainland@apeiron.net>
2022-06-27 11:20:49 -0400

    Add a function to unwrap phase to range [0, 2*pi).

--- src/Math.hh
+++ src/Math.hh
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Drexel University
+// Copyright 2018-2022 Drexel University
 // Author: Geoffrey Mainland <mainland@drexel.edu>
 
 #ifndef MATH_HH_
@@ -8,5 +8,15 @@
 
 /** @brief Convert a floating-point number to a fractional number */
 void frap(double x, long maxden, long &num, long &den);
+
+/** @brief Unwrap phase to [0, 2*M_PI)
+ * @param x Phase
+ * @return wrapped phase
+ */
+inline double unwrapPhase(double x)
+{
+    // This rigamarole is necessary to handle negative values.
+    return fmod(2.0*M_PI + fmod(x, 2.0*M_PI), 2.0*M_PI);
+}
 
 #endif /* MATH_HH_ */
