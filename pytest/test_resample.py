bf60c747ff9c5988155e4bd8de20d0cda999d68c
Geoffrey Mainland <mainland@apeiron.net>
2022-08-16 12:21:37 -0400

    Add more resampling tests.

--- pytest/test_resample.py
+++ pytest/test_resample.py
@@ -6,7 +6,7 @@ from unittest import TestCase
 import numpy as np
 from numpy.typing import ArrayLike
 
-from hypothesis import example, given, settings, strategies as st
+from hypothesis import assume, example, given, settings, strategies as st
 
 import dragonradio.signal
 from dragonradio.signal import Resampler
@@ -39,8 +39,8 @@ class TestResampling(TestCase):
 
         self.assertLessEqual(err, 0.1)
 
-    @given(Fc=st.sampled_from([-450e3, -300e3, -100e3, -50e3, 1e2, 1e3, 100e3, 300e3, 400e3, 450e3]),
-           rate=st.sampled_from([1.0, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 40.0, 50.0]),
+    @given(Fc=st.sampled_from([-450e3, -300e3, -100e3, -50e3, 1e2, 1e3, 10e3, 100e3, 110e3, 300e3, 400e3, 450e3]),
+           rate=st.sampled_from([1/2, 1/3, 2/3, 1/4, 3/4, 1/5, 2/5, 1/10, 1.0, 5/4, 3/2, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 40.0, 50.0]),
            resample=resampler)
     @example(Fc=450e3,
              rate=50.0,
@@ -50,9 +50,11 @@ class TestResampling(TestCase):
         Fs = 1e6
         n = 100000
 
+        assume(abs(Fc) < 0.95*rate*Fs/2)
+
         sig1 = dragonradio.signal.tone(Fc, Fs, n)
         sig2 = dragonradio.signal.tone(Fc, Fs*rate, n*rate)
-        resampled = dragonradio.signal.resample_and_filter(sig1, rate, 0.0, resample=resample, numtaps=701)
+        resampled = dragonradio.signal.resample_and_filter(sig1, rate, 0.0, resample=resample, numtaps=1201)
 
         n = min(len(sig2), len(resampled))
 
