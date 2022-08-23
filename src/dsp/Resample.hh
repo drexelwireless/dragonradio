d15b98803195a457b606e16c44b9acf76d675361
Geoffrey Mainland <mainland@apeiron.net>
2022-08-23 12:08:09 -0400

    Add documentation to Resampler class.

--- src/dsp/Resample.hh
+++ src/dsp/Resample.hh
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Drexel University
+// Copyright 2018-2022 Drexel University
 // Author: Geoffrey Mainland <mainland@drexel.edu>
 
 #ifndef RESAMPLE_HH_
@@ -8,20 +8,38 @@
 
 #include "IQBuffer.hh"
 
+/** @brief Resample a signal
+ * @tparam I Type of input signal values
+ * @tparam O Type of output signal values
+ */
 template <class I, class O>
 class Resampler {
 public:
     Resampler() = default;
     virtual ~Resampler() = default;
 
+    /** @brief Get resampling rate */
     virtual double getRate(void) const = 0;
 
+    /** @brief Get group delay */
     virtual double getDelay(void) const = 0;
 
+    /** @brief Maximum number of output samples produced for given number input
+     * samples
+     * @param count Number of input samples
+     * @return Number of output samples
+     */
     virtual size_t neededOut(size_t count) const = 0;
 
+    /** @brief Reset resampling state */
     virtual void reset(void) = 0;
 
+    /** @brief Resample a signal
+     * @param in Pointer to input samples
+     * @param count Number of input samples
+     * @param out Pointer to output samples
+     * @return Number of output samples produced
+     */
     virtual size_t resample(const I *in, size_t count, O *out) = 0;
 };
 
